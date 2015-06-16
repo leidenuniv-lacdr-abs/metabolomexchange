338ab0635bc4b7f97c7f827887a00d37f1ef921f
Michael van Vliet <info@michaelvanvliet.nl>
2015-06-17 00:43:14 +0200

    Major UI changes: switch to http://materializecss.com Use bower_components

--- app/app.js
+++ app/app.js
@@ -9,9 +9,9 @@ var mx = angular.module('mx', ['ngRoute']);
 mx.config(function($routeProvider) {
     $routeProvider.
     when('/', { templateUrl: '/app/views/home.html', controller: 'MxCtrl' }).
-    when('/search', { templateUrl: '/app/views/search.html', controller: 'MxCtrl' }).    
-    when('/dataset/:provider/:accession', { templateUrl: '/app/views/dataset.html', controller: 'MxCtrl' }).
-    when('/about', { templateUrl: '/app/views/about.html', controller: 'MxCtrl' }).            
+    when('/search', { templateUrl: '/app/views/search.html', controller: 'MxCtrl' }).
+    when('/search/:query', { templateUrl: '/app/views/search.html', controller: 'MxCtrl' }).
+    when('/provider/:provider', { templateUrl: '/app/views/provider.html', controller: 'MxCtrl' }).
     otherwise({ redirectTo: '/' });
 });
 /* ----------------------------*/
@@ -19,53 +19,124 @@ mx.config(function($routeProvider) {
 /**
  * ANGLULAR CONTROLLERS
  **/
-mx.controller('MxCtrl', function ($scope, $routeParams, $http){
-    
-    //getProviders($scope, $http);        
-    
-    if ($routeParams.query){
-        findDatasets($scope, $http, $routeParams.query);
-        console.log($routeParams.query);
-    } else if ($routeParams.provider && $routeParams.accession) {
-        getDataset($scope, $http, $routeParams.provider, $routeParams.accession);
-    } else {
-        getDatasets($scope, $http);
-    }
+mx.controller('MxCtrl', ['$scope', '$routeParams', '$http', '$location', '$anchorScroll', 
+    function ($scope, $routeParams, $http, $location, $anchorScroll){
 
-    function getProviders($scope, $http) {
-    $http.get('http://api.metabolomexchange.org/providers', { cache: true }).
-        success(function(data) {
-            $scope.providers = data;
-        });
-    }
+        // default path to take
+        if (!$location.path){
+            $location.path('/');
+        }
+            
+        // load providers into scope            
+        getProviders($scope, $http);
 
-    function getDataset($scope, $http, provider, accession) {
-    $http.get('http://api.metabolomexchange.org/provider/' + provider + '/' + accession, { cache: true }).
-        success(function(data) {
-            $scope.dataset = data;
-        });
-    }
+        if ($routeParams.query){
+            $scope.query = $routeParams.query;
+            findDatasets($scope, $http, $routeParams.query);
+        } else if ($routeParams.provider && $routeParams.accession) {
+            getDataset($scope, $http, $routeParams.provider, $routeParams.accession);
+        } else if ($routeParams.provider){
+            getProvider($scope, $http, $routeParams.provider);
+        } 
 
-    function getDatasets($scope, $http) {
-    $http.get('http://api.metabolomexchange.org/datasets', { cache: true }).
-        success(function(data) {
-            $scope.datasets = data;
-        });
-    }
+        function getProviders($scope, $http) {
+        $http.get('http://api.metabolomexchange.org/providers', { cache: true }).
+            success(function(data) {
+                $scope.providers = data;
+                console.log("... new call to fetch all providers");
+            });
+        }
 
-    function findDatasets($scope, $http, query) {
-    $http.get('http://api.metabolomexchange.org/datasets/' + query, { cache: true }).
-        success(function(data) {
-            $scope.datasets = data;
-        });
-    }
-}); 
+        function getProvider($scope, $http, provider) {
+        $http.get('http://api.metabolomexchange.org/provider/' + provider, { cache: true }).
+            success(function(data) {
+                $scope.provider = data;
+                $scope.datasets = data.datasets;                
+                console.log("... new call to fetch a single provider " + provider);
+            });
+        } 
 
-mx.controller('SearchCtrl', function ($scope, $location, $routeParams){
-    $scope.changeView = function(view){
-        $location.path(view);
-        $location.search('query', $scope.query); // add query parameter to search scope through a GET param
+        function getDataset($scope, $http, provider, accession) {
+        $http.get('http://api.metabolomexchange.org/provider/' + provider + '/' + accession, { cache: true }).
+            success(function(data) {
+                $scope.dataset = data;
+                console.log("... new call to fetch dataset " + provider + '/' + accession);
+            });
+        }
+
+        function getDatasets($scope, $http) {
+        $http.get('http://api.metabolomexchange.org/datasets', { cache: true }).
+            success(function(data) {
+                $scope.datasets = data;
+                console.log("... new call to fetch all datasets");
+            });
+        }
+
+        function findDatasets($scope, $http, query) {
+        var andMatch = query.replace(new RegExp(' ', 'g'), '&'); // re-format to call the API with a AND match result
+        $http.get('http://api.metabolomexchange.org/datasets/' + andMatch, { cache: true }).
+            success(function(data) {
+                $scope.datasets = data;
+                console.log("... new call to fetch all (" + data.length + ") datasets that match " + andMatch);
+            });
+        }
+
+        $scope.updateResults = function(){
+            console.log('updateResults on path', $location.path());
+            if (!$scope.query && $location.path() != '/'){
+                $location.path('/');
+                $location.hash('top');
+                $anchorScroll();                 
+            }                        
+            if ($scope.query && $location.path() == '/'){
+                $location.path('search');
+                $location.hash('top');
+                $anchorScroll();                 
+            }                   
+
+            findDatasets($scope, $http, $scope.query);
+            $location.search($scope.query); // update url
+            $location.hash('top'); // scroll to top where the results start
+            $anchorScroll();            
+        } 
+
+        /**
+         * ANCHORS
+         **/
+        $scope.scrollTo = function(id) {
+            $location.hash(id);
+            $anchorScroll();
+        }
+
+        /**
+         * VIEW Switcher
+         **/
+        $scope.changeView = function(view){
+            if ($location.path != view){
+                $location.path(view);
+                $location.hash('top');
+                $anchorScroll();            
+            }
+        }
     }
-}); 
-   
+]); 
+
+mx.directive('focus', function($timeout) {
+    return {
+        scope : {
+            trigger : '@focus'
+        },
+        link : function(scope, element) {
+            scope.$watch('trigger', function(value) {
+                if (value === "true") {
+                    $timeout(function() {
+                        element[0].focus();
+                    });
+                }
+            });
+        }
+    };
+
+});
+
 /* ----------------------------*/
--- views/home.php
+++ views/home.php
@@ -8,76 +8,137 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <title>MetabolomeXchange</title>
-    <meta name="description" content="metabolomics data sharing">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" href="/favicon.png">
-    <link href='http://fonts.googleapis.com/css?family=Shadows+Into+Light' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="/css/normalize.min.css">
-    <link rel="stylesheet" href="/css/main.css">
+
+    <link rel="stylesheet" href="bower_components/materialize/dist/css/materialize.min.css">
+    <link rel="stylesheet" href="css/style.css">
 
     <script type="text/javascript">
-      // detect IE 9 or higher
       var ie = (function(){ var undef, v = 3, div = document.createElement('div'), all = div.getElementsByTagName('i'); while ( div.innerHTML = '<!--[if gt IE ' + (++v) + ']><i></i><![endif]-->', all[0] ); return v > 4 ? v : undef; }());
       if (ie && ie <= 8){ window.location.replace("/static"); }
-    </script>    
+    </script>
 
-    <script src="/js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>    
-
-    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular-route.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular-cookies.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular-sanitize.min.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular-animate.min.js"></script>
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>    
-
-    <script src="/app/app.js"></script>    
-
-    <script src="/js/vendor/d3.v3.min.js"></script>
-    <script src="/js/vendor/dimple.v2.0.0.min.js"></script>        
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
 </head>
-<body OnLoad="document.searchform.search.focus();">
-    <div class="content">
-      <div class="header">
-        <div class="header_content">
-            <div class="logo">
-              <a href="/#/">metabolome<font style="font-size: 1.7em; color: #2a7640">X</font>change</a>
-            </div>
-            <div class="menu"> 
-              <div class="menu_item"><a href="#/">Home</a></div>
-              <div class="menu_item"><a href="#/search">Search</a></div>              
-              <div class="menu_item"><a href="#/about">About</a></div>
-            </div>
-            <br class="clearBoth" />
+<body ng-controller="MxCtrl">
+
+  <a name="top"></a>
+
+  <ul id="dropdown" class="dropdown-content">
+  <li><a href="" ng-click="scrollTo('about')">About</a></li>
+  <li><a href="" ng-click="scrollTo('about')">Partners</a></li>
+  <li><a href="" ng-click="changeView('search')">Search</a></li>
+  <li class="divider"></li>
+  </ul>  
+
+  <nav class="teal darken-4" role="navigation">
+    <div class="nav-wrapper container">
+      <a id="logo-container" href="#/" class="left">
+        <i class="mdi-action-home"><img class="hide-on-med-and-up" style="margin: 15px 0 -4px 15px; height:30px;" src="/img/metabolomeXchange.png"></i>
+      </a>    
+      <ul class="right">
+        <li><a class="dropdown-button" href="#!" data-activates="dropdown"><i class="mdi-navigation-menu"></i></a></li>
+      </ul>    
+    </div>
+  </nav>
+
+  <div class="section no-pad-bot" id="index-banner">
+    <div class="container">
+      <div class="row center">
+        <div class="hide-on-small-only">
+          <br />
         </div>
+        <img class="hide-on-small-only" style="max-width:100%; max-height:91px;" src="/img/metabolomeXchange.png" />
+        <h5 class="header col s12 light">An international data aggregation and notification service for metabolomics.</h5>
       </div>
-      <div class="search">
-        <div class="search_content">
-          <form ng-controller="SearchCtrl" action="#/search" name="searchform" method="POST" class="form-wrapper cf">
-            <input ng-focus="changeView('search')" ng-change="changeView('search')" ng-model="query" type="text" name="search" value="" placeholder="Search here..." required>
-            <button type="submit" disabled>Search</button>
-          </form>
-          <br class="clearBoth" />
-        </div>
-      </div>          
-      <div class="content_main"> 
-        <div ng-view></div>
-      </div>
-      <div class="mx_footer">
-        <div class="mx_footer_content">
-          <ul class="share-buttons">
-            <li><a href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fmetabolomexchange.org&t=MetabolomeXchange" target="_blank" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(document.URL) + '&t=' + encodeURIComponent(document.URL)); return false;"><img src="/img/flat_web_icon_set/black/Facebook.png"></a></li>
-            <li><a href="https://twitter.com/intent/tweet?source=http%3A%2F%2Fmetabolomexchange.org&text=MetabolomeXchange: http%3A%2F%2Fmetabolomexchange.org" target="_blank" title="Tweet" onclick="window.open('https://twitter.com/intent/tweet?text=' + encodeURIComponent(document.title) + ': ' + encodeURIComponent(document.URL) + ' %23metabolomics' + ' %40metabolomexchan'); return false;"><img src="/img/flat_web_icon_set/black/Twitter.png"></a></li>
-            <li><a href="https://plus.google.com/share?url=http%3A%2F%2Fmetabolomexchange.org" target="_blank" title="Share on Google+" onclick="window.open('https://plus.google.com/share?url=' + encodeURIComponent(document.URL)); return false;"><img src="/img/flat_web_icon_set/black/Google+.png"></a></li>
-            <li><a href="http://www.linkedin.com/shareArticle?mini=true&url=http%3A%2F%2Fmetabolomexchange.org&title=MetabolomeXchange,%20an%20international%20initiative%20to%20promote%20and%20facilitate%20sharing%20of%20Metabolomics%20data.&source=http%3A%2F%2Fmetabolomexchange.org" target="_blank" title="Share on LinkedIn" onclick="window.open('http://www.linkedin.com/shareArticle?mini=true&url=' + encodeURIComponent(document.URL) + '&title=' +  encodeURIComponent(document.title)'); return false;"><img src="/img/flat_web_icon_set/black/LinkedIn.png"></a></li>
-            <li><a href="mailto:?subject=MetabolomeXchange&body=MetabolomeXchange,%20an%20international%20initiative%20to%20promote%20and%20facilitate%20sharing%20of%20Metabolomics%20data.: http%3A%2F%2Fmetabolomexchange.org" target="_blank" title="Email" onclick="window.open('mailto:?subject=' + encodeURIComponent(document.title) + '&body=' +  encodeURIComponent(document.URL)); return false;"><img src="/img/flat_web_icon_set/black/Email.png"></a></li>
-          </ul>
-          <a target="_blank" href="http://www.cosmos-fp7.eu/"><img height="40px" class="mx_logo" src="/img/cosmos/fp7.png"></a> This project is funded through European Commission COSMOS Grant EC312941
-          <div>
-            <a target="_blank" href="/ns/dcat">RDF</a>
+      <div class="row center">
+          <div class="white teal-text" role="content">
+              <form class="col s12">
+                <div class="row">
+                    <div class="input-field col s2"> </div>                                      
+                    <div class="input-field col s8">
+                        <i class="mdi-action-search prefix black-text"></i>
+                        <input id="icon_prefix" type="text" class="validate" ng-focus="changeView('search');" ng-change="updateResults();" ng-model="query" value="{{query}}" placeholder="What are you looking for?">
+                        <label class="black-text" for="icon_prefix">Search</label>
+                    </div>  
+                    <div class="input-field col s2"> </div>                                      
+                </div>
+              </form>
           </div>
+      </div>      
+    </div>
+  </div>  
+
+  <div ng-view></div>
+
+  <br/><br/>
+  <footer class="page-footer teal darken-2">
+    <div class="container">
+      <div class="row">
+        <div class="col l6 s12">
+          <a name="about"></a>
+          <h5 class="white-text">About</h5>
+          <p class="grey-text text-lighten-4"><a target="_blank" href="http://www.cosmos-fp7.eu/"><img height="40px" class="mx_logo" src="/img/cosmos/fp7.png"></a> This project is funded through European Commission COSMOS Grant EC312941</p>
+          <p>
+            <a href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fmetabolomexchange.org&t=MetabolomeXchange" target="_blank" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u=' + encodeURIComponent(document.URL) + '&t=' + encodeURIComponent(document.URL)); return false;"><img src="/img/flat_web_icon_set/black/Facebook.png"></a>
+            <a href="https://twitter.com/intent/tweet?source=http%3A%2F%2Fmetabolomexchange.org&text=MetabolomeXchange: http%3A%2F%2Fmetabolomexchange.org" target="_blank" title="Tweet" onclick="window.open('https://twitter.com/intent/tweet?text=' + encodeURIComponent(document.title) + ': ' + encodeURIComponent(document.URL) + ' %23metabolomics' + ' %40metabolomexchan'); return false;"><img src="/img/flat_web_icon_set/black/Twitter.png"></a>
+            <a href="https://plus.google.com/share?url=http%3A%2F%2Fmetabolomexchange.org" target="_blank" title="Share on Google+" onclick="window.open('https://plus.google.com/share?url=' + encodeURIComponent(document.URL)); return false;"><img src="/img/flat_web_icon_set/black/Google+.png"></a>
+            <a href="http://www.linkedin.com/shareArticle?mini=true&url=http%3A%2F%2Fmetabolomexchange.org&title=MetabolomeXchange,%20an%20international%20initiative%20to%20promote%20and%20facilitate%20sharing%20of%20Metabolomics%20data.&source=http%3A%2F%2Fmetabolomexchange.org" target="_blank" title="Share on LinkedIn" onclick="window.open('http://www.linkedin.com/shareArticle?mini=true&url=' + encodeURIComponent(document.URL) + '&title=' +  encodeURIComponent(document.title)'); return false;"><img src="/img/flat_web_icon_set/black/LinkedIn.png"></a>
+            <a href="mailto:?subject=MetabolomeXchange&body=MetabolomeXchange,%20an%20international%20initiative%20to%20promote%20and%20facilitate%20sharing%20of%20Metabolomics%20data.: http%3A%2F%2Fmetabolomexchange.org" target="_blank" title="Email" onclick="window.open('mailto:?subject=' + encodeURIComponent(document.title) + '&body=' +  encodeURIComponent(document.URL)); return false;"><img src="/img/flat_web_icon_set/black/Email.png"></a>
+          </p>
+
+        </div>
+        <div class="col l3 s12">
+          <h5 class="orange-text">Partners</h5>
+          <ul>
+            <li><a class="white-text" target="_blank" href="#!">Leiden University</a></li>
+            <li><a class="white-text" target="_blank" href="#!">EMBL-EBI</a></li>
+            <li><a class="white-text" href="/ipb">Leibniz-Institute of Plant Biochemistry</a></li>
+          </ul>
+          <h5 class="orange-text">Providers</h5>
+          <ul>
+            <li><a class="white-text" href="#!">EMBL-EBI</a></li>
+            <li><a class="white-text" href="/maxplanck">Max Planck Institute - MPIMP</a></li>
+            <li><a class="white-text" href="">UC San Diego</a></li>
+            <li><a class="white-text" href="/bordeaux">Universite Bordeaux Segalen</a></li>            
+          </ul>          
+        </div>
+        <div class="col l3 s12">
+          <h5 class="orange-text">Contributors</h5>          
+          <ul>
+            <li><a class="white-text" href="/MRC">MRC-HNR</a></li>
+            <li><a class="white-text" href="/imperial">Imperial College</a></li>
+            <li><a class="white-text" href="/tno">TNO</a></li>
+            <li><a class="white-text" href="/vtt">Teknologian Tutkimuskeskus VTT</a></li>
+            <li><a class="white-text" href="/barcelona">Universitat De Barcelona</a></li>
+            <li><a class="white-text" href="/manchester">The University Of Manchester</a></li>
+            <li><a class="white-text" href="/cirmmp">CIRMMP</a></li>
+            <li><a class="white-text" href="/birmingham">The University Of Birmingham</a></li>
+            <li><a class="white-text" href="/oxford">The University of Oxford</a></li>
+          </ul>          
         </div>
       </div>
     </div>
+    <div class="footer-copyright">
+      <div class="container">
+      Made with: 
+      <a class="orange-text text-lighten-3" target="_blank" href="https://angularjs.org/">AngularJS (client)</a>, 
+      <a class="orange-text text-lighten-3" target="_blank" href="http://materializecss.com">Materialize (design/ui)</a>, and 
+      <a class="orange-text text-lighten-3" target="_blank" href="http://materializecss.com">PHP/FlightPHP (api/rss)</a>
+      </div>
+    </div>
+  </footer>
+
+    <script src="bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="bower_components/materialize/dist/js/materialize.min.js"></script>    
+    <script src="bower_components/angular/angular.min.js"></script>
+    <script src="bower_components/angular-route/angular-route.min.js"></script>
+
+    <script src="app/app.js"></script> 
+
+    <br /><br /><br /><br />      
+    <br /><br /><br /><br />
+    <br /><br /><br /><br />
+    <br /><br /><br /><br />
+    <br /><br /><br /><br />
   </body>
 </html>
