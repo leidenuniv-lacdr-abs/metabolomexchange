88f5f93817c7df1a85c59b266e6eb14eed4615a8
Michael van Vliet <info@michaelvanvliet.nl>
2015-08-14 23:52:48 +0200

    Bower update Added stats using D3 NVD3

--- site/app/app.js
+++ site/app/app.js
@@ -1,7 +1,7 @@
 /**
  * ANGLULAR APP
  **/
-var mx = angular.module('mx', ['ngRoute']);
+var mx = angular.module('mx', ['ngRoute', 'nvd3']);
 
 // $.getJSON('http://api.metabolomexchange.org/providers', function(data) {
 //     for (var i in data) {
@@ -30,8 +30,90 @@ mx.controller('MxController', ['$scope', '$routeParams', '$location', '$anchorSc
     function ($scope, $routeParams, $location, $anchorScroll, mxApi){ 
 
         $scope.doneLoading = '0'; 
-
         $scope.datasets = [];
+
+        $scope.options = {
+            chart: {
+                type: 'multiBarChart',
+                height: 450,
+                margin : {
+                    top: 20,
+                    right: 20,
+                    bottom: 60,
+                    left: 45
+                },
+                clipEdge: true,
+                staggerLabels: true,
+                transitionDuration: 500,
+                stacked: true,
+                xAxis: {
+                    axisLabel: 'Date',
+                    showMaxMin: false,
+                    tickFormat: function(d){
+                        return d3.time.format('%m/%y')(new Date(d*1000))
+                    }
+                },
+                yAxis: {
+                    axisLabel: 'Datasets',
+                    axisLabelDistance: 40,
+                    showMaxMin: true,
+                    tickFormat: function(d){
+                        return d3.format('03d')(d);
+                    }
+                }
+            }
+        };
+
+        //$scope.data = generateData();
+        mxApi.getStats().then(function(d) {  
+            $scope.stats = d.data;             
+        });
+
+        /* Random Data Generator (took from nvd3.org) */
+        function generateData() {
+            return stream_layers(4,50+Math.random()*50,.1).map(function(data, i) {
+                return {
+                    key: 'Provider ' + i,
+                    values: data
+                };
+            });
+        }
+
+        /* Inspired by Lee Byron's test data generator. */
+        function stream_layers(n, m, o) {
+            if (arguments.length < 3) o = 0;
+            function bump(a) {
+                var x = 1 / (.1 + Math.random()),
+                    y = 2 * Math.random() - .5,
+                    z = 10 / (.1 + Math.random());
+                for (var i = 0; i < m; i++) {
+                    var w = (i / m - y) * z;
+                    a[i] += x * Math.exp(-w * w);
+                }
+            }
+            return d3.range(n).map(function() {
+                var a = [], i;
+                for (i = 0; i < m; i++) a[i] = o + o * Math.random();
+                for (i = 0; i < 5; i++) bump(a);
+                return a.map(stream_index);
+            });
+        }
+
+        /* Another layer generator using gamma distributions. */
+        function stream_waves(n, m) {
+            return d3.range(n).map(function(i) {
+                return d3.range(m).map(function(j) {
+                    var x = 20 * j / m - i / 3;
+                    return 2 * x * Math.exp(-.5 * x);
+                }).map(stream_index);
+            });
+        }
+
+        function stream_index(d, i) {
+            return {x: i, y: Math.max(0, d)};
+        }
+
+            
 
         if ($routeParams.search){ $scope.search = $routeParams.search; }
         if (!$scope.search) { $scope.search = ''; }
@@ -98,5 +180,6 @@ mx.factory('mxApi', function($http) {
         }
     };
 });
+
 
 /* ----------------------------*/
